53b798bfc4afcc89d8aa0b487a790fc438b8b5c3
Matej <matej.novosad.mn@gmail.com>
2021-12-25 17:38:13 +0100

    second try

---
+++ main.py
@@ -0,0 +1,5 @@
+# Gather data about every single user on a server
+# - gather mutual servers of each user user_id:{'guilds':{guild_id:"name of guild"}}, 'name':Carrot#3900}
+import requests
+from bs4 import
+
