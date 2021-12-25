78f2d80088c4088419c507480b249dc023cf7719
Matej <matej.novosad.mn@gmail.com>
2021-12-25 19:27:31 +0100

    collected servers

--- main.py
+++ main.py
@@ -4,17 +4,40 @@ import requests
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
 
 def setup_driver(url: str):
     options = webdriver.ChromeOptions()
     options.add_argument("start-minimized")
     options.add_argument("disable-infobars")
     options.add_argument("--disable-extensions")
-    driver = webdriver.Firefox(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(url)
     return driver
 
 
+def login(driver):
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.url_changes("https://discord.com/login?redirect_to=%2Fchannels%2F%40me"))
+    wait.until(EC.url_changes("https://discord.com/channels/@me"))
+    return driver
+
+def server_list(soup):
+    elements = soup.find_all('div', {'class': 'wrapper-1BJsBx'})
+    return {ele['href'] for ele in elements[1:]} # get all the urls to servers and ignore @me server
+
 if __name__ == '__main__':
     driver = setup_driver("https://discord.com/channels/@me")
+    driver = login(driver)
+    time.sleep(3)
+    source = driver.page_source
+    print(source)
+
+    soup = BeautifulSoup(source, "html.parser")
+    server_list = server_list(soup)
+
 
