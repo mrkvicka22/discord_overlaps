ebd64897f8be3af71b6f7f0f893c0839596b7a6a
Matej <matej.novosad.mn@gmail.com>
2021-12-26 01:12:09 +0100

    added getting users to dictionaries

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.touch_actions import TouchActions
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 
 
@@ -14,6 +19,7 @@ def setup_driver(url: str):
     options = webdriver.ChromeOptions()
     options.add_argument("start-minimized")
     options.add_argument("disable-infobars")
+    #options.add_experimental_option('w3c', False)
     options.add_argument("--disable-extensions")
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
     driver.get(url)
@@ -26,18 +32,46 @@ def login(driver):
     wait.until(EC.url_changes("https://discord.com/channels/@me"))
     return driver
 
-def server_list(soup):
+
+def get_server_list(soup):
     elements = soup.find_all('div', {'class': 'wrapper-1BJsBx'})
-    return {ele['href'] for ele in elements[1:]} # get all the urls to servers and ignore @me server
+    return {ele['href'] for ele in elements[1:]}  # get all the urls to servers and ignore @me server
+
+
+def get_users(soup, driver, user_set):
+    last_l = len(user_set)
+    ignored_exceptions = (NoSuchElementException, StaleElementReferenceException,)
+    wait = WebDriverWait(driver, 3)
+    elements = driver.find_elements_by_class_name('wrapper-3t9DeA')
+    for ele in elements:
+        user_set.add(ele.get_attribute('aria-label').split(',')[0])
+
+    while len(user_set) > last_l:
+        last_l = len(user_set)
+        wait.until(EC.element_to_be_clickable(elements[-1]))
+        elements[-1].click()
+        elements = driver.find_elements_by_class_name('wrapper-3t9DeA')
+        for ele in elements:
+            user_set.add(ele.get_attribute('aria-label').split(',')[0])
+    print(user_set)
+
+
+
+    #print([ele['aria-label'].split(',')[0] for ele in elements[1:]])
+
+    #TouchActions(driver).scroll_from_element(elements[-1], yoffset=30*len(elements), xoffset=0).perform()
 
 if __name__ == '__main__':
     driver = setup_driver("https://discord.com/channels/@me")
     driver = login(driver)
     time.sleep(3)
     source = driver.page_source
-    print(source)
-
+    user_set = set()
     soup = BeautifulSoup(source, "html.parser")
-    server_list = server_list(soup)
-
-
+    server_list = get_server_list(soup)
+    for server_href in server_list:
+        driver.get("https://discord.com" + server_href)
+        time.sleep(10)
+        server_source = driver.page_source
+        server_soup = BeautifulSoup(server_source, "html.parser")
+        get_users(server_soup, driver, user_set)
